48a3821fceec81c1455bfaf36f2bb0b6b55d455f
dsig <carlos.poiares2@gmail.com>
2019-08-16 00:49:09 +0100

    text test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# gbpNote
-Bot to notify me when it's time to convert my last EUR to GBP
+## GBP - Note
+Bot to notify me when it's time to convert my last EUR to GBP - Sucks Ass
+- Twillio integration - https://www.twilio.com/docs/libraries/python
--- coinbot.py
+++ coinbot.py
@@ -1,14 +1,21 @@
+from twilio.rest import Client
 import json
 import requests
 import time
-
+ 
 gbp_url = "https://api.coindesk.com/v1/bpi/currentprice/GBP.json"
 eur_url = "https://api.coindesk.com/v1/bpi/currentprice/EUR.json"
+accountSid = 'AC3eb64f569aa98aaf3b9d2e78160c2602'
+authToken = 'db2735f8b71f144e7f7eeecc4106de61'
+
+twilioClient = Client(accountSid, authToken)
+myNumber = '441668932047'
+destNumber = '447305665218'
 
 def check_value(value):
     if(value < 1):
         return True
-    elif(value > 1 & value - 0.07 <= 1):
+    elif((value > 1) & (value - 0.07 <= 1)):
         return True
     return False
 
@@ -17,10 +24,12 @@ def get_value(string):
     aux = aux[0] + aux[1]
     return float(aux)
     
-
 def send_msg(eur_gbp_value):
-    return
-
+    message = 'OLHA AI O PREÇO DA POUND CRL: %0.5f\n TROCA AI!!!' % (eur_gbp_value) 
+    print("Sending Text... %s\n" % message)
+    myMessage = twilioClient.messages.create(body = message, from_=myNumber, to=destNumber)
+    print("Text Sent %s - %s\n" % (myMessage.sid, myMessage.body))
+    
 def main_loop():
     request = requests.get(gbp_url)
     results_gbp = request.json()
@@ -33,8 +42,8 @@ def main_loop():
     eur_gbp_value = eur_rate/gbp_rate
     if(check_value(eur_gbp_value)):
         send_msg(eur_gbp_value)
-    print(eur_gbp_value)
-    time.sleep(5)
+    print('CURRENT VALUE - %0.5f\n' % eur_gbp_value)
+    time.sleep(500)
     main_loop()
 
 if __name__ == '__main__':
